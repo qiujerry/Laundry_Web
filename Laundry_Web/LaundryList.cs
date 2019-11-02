a3cd30309adffad561a57000ed3f9d13a45a8e4f
itenpn <iten.pn@gmail.com>
2019-11-02 14:38:27 -0400

    Implemented Test Backend

--- Laundry_Web/LaundryList.cs
+++ Laundry_Web/LaundryList.cs
@@ -4,12 +4,9 @@ namespace Laundry_Web
 {
     public class LaundryList
     {
+        public int MachineNumber { get; set; }
+
         public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string Summary { get; set; }
     }
 }
